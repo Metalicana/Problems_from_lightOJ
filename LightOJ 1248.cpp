f4f5574153063109a0ff8d7051588c7af119d9e1
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2020-05-14 18:02:31 +0600

    Add files via upload

---
+++ LightOJ 1248.cpp
@@ -0,0 +1,46 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define pll pair<long long, long long >
+#define ll long long
+#define lcm(a,b) (a)/__gcd((a),(b))*(b)
+#define pb push_back
+#define mp make_pair
+#define x first
+#define y second
+#define maxn 100000
+#define mod 1000000007
+#define endl '\n'
+#define trace(x) cerr << #x << ": " << x << endl;
+#define trace2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+
+int dir8[2][8] = {{1,-1,0,0,1,-1,1,-1},{0,0,1,-1,1,-1,-1,1}};
+int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+int m,n,row,col;
+void solve(int test)
+{
+  scanf("%d",&n);
+  double x = 0.0;
+  for(int i=n-1;i>=0;i--)
+  {
+    x = x + 1 + ((double)i)/(double)(n-i);
+  }
+  printf("Case %d: %.10lf\n",test,x);
+
+}
+void test()
+{
+  int t;
+  scanf("%d",&t);
+  for(int test=1;test<=t;test++)
+  {
+    solve(test);
+  }
+}
+int main()
+{
+  test();
+  return 0;
+}
