d918ecf9967c8c609a261ce58f3ef7aec40899f7
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2020-08-09 08:33:56 +0600

    Add files via upload

---
+++ LightOJ 1374.cpp
@@ -0,0 +1,63 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define pll pair<long long, long long >
+#define ll long long
+#define lcm(a,b) (a)/__gcd((a),(b))*(b)
+#define pb push_back
+#define sz size()
+#define mp make_pair
+#define rep(i,n) for(i=0;i<(n);i++)
+#define x first
+#define y second
+#define maxn 100000
+#define MOD 1000000007
+#define pi (2.0*acos(0.0))
+#define endl '\n'
+#define trace(x) cerr << #x << ": " << x << endl;
+#define trace2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+int dir8[2][8] = {{1,-1,0,0,1,-1,1,-1},{0,0,1,-1,1,-1,-1,1}};
+int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+int m,n,row,col;
+int ara[10005];
+int counter[10005];
+void solve(int test)
+{
+  memset(counter,0,sizeof counter);
+  scanf("%d",&n);
+  bool k = 0;
+  for(int i=1;i<=n;i++)
+  {
+    scanf("%d",&m);
+    if(m >= n)k=1;
+    counter[m]++;
+  }
+  for(int i=0;i<n/2;i++)
+  {
+    if(counter[i] + counter[n-i-1] != 2)
+    {
+      k = 1;
+      break;
+    }
+  }
+  if(!k)printf("Case %d: yes\n",test);
+  else printf("Case %d: no\n",test);
+
+}
+void test()
+{
+  int t;
+  scanf("%d",&t);
+  for(int test=1;test<=t;test++)
+  {
+    solve(test);
+  }
+}
+int main()
+{
+  //freopen("o.txt","w",stdout);
+  test();
+  return 0;
+}
