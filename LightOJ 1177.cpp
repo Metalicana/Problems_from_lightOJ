aa498fdf6e91e464a11d0d56a903c58d16c3f262
Abdul Mohaimen Al Radi <alradi9923@gmail.com>
2021-03-07 07:11:56 +0600

    Add files via upload

---
+++ LightOJ 1177.cpp
@@ -0,0 +1,199 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define pii pair<int,int>
+#define pll pair<long long, long long >
+#define ll long long
+#define lcm(a,b) (a)/__gcd((a),(b))*(b)
+#define pb push_back
+#define sz size()
+#define mp make_pair
+#define rep(i,n) for(i=0;i<(n);i++)
+#define x first
+#define y second
+#define eps 1e-9
+#define maxn 100000
+#define MOD 1000000007
+#define pi (2.0*acos(0.0))
+#define endl '\n'
+#define trace(x) cerr << #x << ": " << x << endl;
+#define trace2(x, y) cerr << #x << ": " << x << " | " << #y << ": " << y << endl;
+#define trace3(x, y, z) cerr << #x << ": " << x << " | " << #y << ": " << y << " | " << #z << ": " << z << endl;
+#define trace4(a, b, c, d)cerr << #a << ": " << a << " | " << #b << ": " << b << " | " << #c << ": " << c << " | " << #d << ": " << d << endl;
+bool equalTo ( double a, double b ){ if ( fabs ( a - b ) <= eps ) return true; else return false; }
+bool notEqual ( double a, double b ){if ( fabs ( a - b ) > eps ) return true; else return false; }
+bool lessThan ( double a, double b ){ if ( a + eps < b ) return true; else return false; }
+bool lessThanEqual ( double a, double b ){if ( a < b + eps ) return true;   else return false;}
+bool greaterThan ( double a, double b ){if ( a > b + eps ) return true;else return false;}
+bool greaterThanEqual ( double a, double b ){if ( a + eps > b ) return true;else return false;}
+bool check(int mask,int i){return ((1<<i)&mask);}
+int Set(int mask,int i){return mask | (1<<i);}
+template<class T>
+T fast_pow(T n , T p)
+{
+    if(p==0) return 1;
+    if(p%2)
+    {
+        T g=mod_v ( mod_v(n) * mod_v(fast_pow(n,p-1)) );
+        return g;
+    }
+    else
+    {
+        T g=fast_pow(n,p/2);
+        g=mod_v( mod_v(g) * mod_v(g) ) ;
+        return g;
+    }
+}
+
+template<class T>
+inline T modInverse(T n)
+{
+    return fast_pow(n,MOD-2);
+}
+
+ll add(ll a, ll b)
+{
+  if(a+b<MOD)return a+b;
+  return (a+b)%MOD;
+}
+ll mul(ll a, ll b)
+{
+  if(a*b < MOD)return a*b;
+  return (a*b)%MOD;
+}
+int dir8[2][8] = {{1,-1,0,0,1,-1,1,-1},{0,0,1,-1,1,-1,-1,1}};
+int dir4[2][4] = {{1,-1,0,0},{0,0,1,-1}};
+
+//TEMPLATE ENDS HERE, SO DOES MY WILL TO LIVE
+struct FlowEdge {
+    int v, u;
+    long long cap, flow = 0;
+    FlowEdge(int v, int u, long long cap) : v(v), u(u), cap(cap) {}
+};
+
+struct Dinic {
+    const long long flow_inf = 1e18;
+    vector<FlowEdge> edges;
+    vector<vector<int>> adj;
+    int n, m = 0;
+    int s, t;
+    vector<int> level, ptr;
+    queue<int> q;
+
+    Dinic(int n, int s, int t) : n(n), s(s), t(t) {
+        adj.resize(n);
+        level.resize(n);
+        ptr.resize(n);
+    }
+    void reset()
+    {
+      edges.clear();
+      for(int i=0;i<maxn;i++)adj.clear();
+      adj.resize(n);
+      level.resize(n);
+      ptr.resize(n);
+      m=0;
+    }
+    void add_edge(int v, int u, long long cap) {
+        edges.emplace_back(v, u, cap);
+        edges.emplace_back(u, v, 0);
+        adj[v].push_back(m);
+        adj[u].push_back(m + 1);
+        m += 2;
+    }
+
+    bool bfs() {
+        while (!q.empty()) {
+            int v = q.front();
+            q.pop();
+            //trace(adj[v].sz)
+            for (int id : adj[v]) {
+              //trace(edges[id].u)
+                if (edges[id].cap - edges[id].flow < 1)
+                    continue;
+                if (level[edges[id].u] != -1)
+                    continue;
+                level[edges[id].u] = level[v] + 1;
+                q.push(edges[id].u);
+            }
+        }
+        return level[t] != -1;
+    }
+
+    long long dfs(int v, long long pushed) {
+        if (pushed == 0)
+            return 0;
+        if (v == t)
+            return pushed;
+        for (int& cid = ptr[v]; cid < (int)adj[v].size(); cid++) {
+            int id = adj[v][cid];
+            int u = edges[id].u;
+            if (level[v] + 1 != level[u] || edges[id].cap - edges[id].flow < 1)
+                continue;
+            long long tr = dfs(u, min(pushed, edges[id].cap - edges[id].flow));
+            if (tr == 0)
+                continue;
+            edges[id].flow += tr;
+            edges[id ^ 1].flow -= tr;
+            return tr;
+        }
+        return 0;
+    }
+
+    long long flow() {
+        long long f = 0;
+        while (true) {
+            fill(level.begin(), level.end(), -1);
+            level[s] = 0;
+            q.push(s);
+            if (!bfs())
+            {
+                break;
+            }
+            fill(ptr.begin(), ptr.end(), 0);
+            while (long long pushed = dfs(s, flow_inf)) {
+                f += pushed;
+            }
+        }
+        return f;
+    }
+};
+
+Dinic dinic = Dinic(105,0,0);
+int m,n,row,col,w,u,v,c;
+
+void solve(int test)
+{
+  dinic.reset();
+  scanf("%d%d",&m,&w);
+  dinic.add_edge(1,m+1,INT_MAX);
+  dinic.add_edge(m,m+m,INT_MAX);
+  for(int i=2;i<m;i++)
+  {
+    scanf("%d",&row);
+    dinic.add_edge(i,i+m,row);
+  }
+  while(w--)
+  {
+    scanf("%d%d%d",&u,&v,&c);
+    dinic.add_edge(u+m,v,c);
+    dinic.add_edge(v+m,u,c);
+  }
+  dinic.s = 1;
+  dinic.t = 2*m;
+  printf("Case %d: %lld\n",test, dinic.flow());
+}
+void test()
+{
+  int t;
+  scanf("%d",&t);
+  for(int test=1;test<=t;test++)
+  {
+    solve(test);
+  }
+}
+int main()
+{
+  //freopen("o.txt","w",stdout);
+  test();
+  return 0;
+}
